b0e167ee2ae0982ba85a07be6c06721abf78b417
sujan698 <sujanbhattarai699@gmail.com>
2025-04-12 00:04:49 +0545

    Initialized the frontend and created a file structure

--- frontend/src/redux/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import productReducer from "./slices/productSlice"
-export default configureStore({
-    reducer:{
-        //add reducer here
-        products:productReducer,
-    },
-})
